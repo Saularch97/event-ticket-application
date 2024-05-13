d1737eed3cea0e392c5ef1169ac4dc87557c2bb7
Saularch97 <ice_ssantos@uolinc.com>
2024-05-13 20:02:52 -0300

    Readme and base repositories created

--- README.md
+++ README.md
@@ -8,7 +8,6 @@ Steps to run the project in your local machine
 - run the command `docker compose up` (need to have docker desktop running if you are mac user)
 - then run via terminal() or Intelij
 
-
 To access the database via terminal:
 
 ` docker exec -it booking-clean-arch-postgres-1 psql -U myuser -d mydatabase
@@ -18,4 +17,15 @@ Some useful commands:
 `\c` --> connect to a database
 `\dt` --> list tables available
 
-
+To access the database via PgAdmin:
+- Access pg admin in this link(docker must be running) [link](http://localhost:5050/)
+- Register a server
+  ![img.png](img.png)
+  - Then go to connection
+  ![img_1.png](img_1.png)
+  - You need to fill those 
+    - To get the Host name/address you need to:
+      - Run `docker ps`
+      - Get the port with `docker inspect <ID_FROM_PG_ADMIN_CONTAINER> | grep IPAddress`
+  ![img_2.png](img_2.png)
+      
--- src/main/java/com/example/booking/BookingApplication.java
+++ src/main/java/com/example/booking/BookingApplication.java
@@ -1,48 +1,14 @@
 package com.example.booking;
 
-import com.example.booking.entities.Role;
-import com.example.booking.entities.Ticket;
-import com.example.booking.entities.User;
-import com.example.booking.repository.TicketRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import java.time.LocalDateTime;
-import java.util.Set;
-import java.util.UUID;
 
 @SpringBootApplication
 public class BookingApplication {
-
-	private static final Logger log = LoggerFactory.getLogger(BookingApplication.class);
-
 	public static void main(String[] args) {
 		SpringApplication.run(BookingApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner commandLineRunner(TicketRepository repository) {
-		return args -> {
-			if(repository.count() == 0) {
-
-				Set<Role> roles = Set.of(new Role(1L, Role.Values.ADMIN.name()));
-
-				Ticket ticket = new Ticket(
-						null,
-						new User(null, "Ronaldo", "ronaldo123", null),
-						"Show Gustavo Lima",
-						"Vila Teixeira",
-						LocalDateTime.of(2024, 4, 3, 16, 20),
-						345.5
-				);
-
-				repository.save(ticket);
-				log.info("Ingresso cadastrado com sucesso! " + ticket.getTicketName());
-			}
-		};
 	}
 }
