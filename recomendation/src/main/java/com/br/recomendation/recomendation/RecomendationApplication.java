109ac23e209ab7650bc1997abb9a09cd598d56c1
Saularch97 <duart.herrera@gmail.com>
2025-05-26 23:53:32 -0300

    microsservice for recomendation created

---
+++ recomendation/src/main/java/com/br/recomendation/recomendation/RecomendationApplication.java
@@ -0,0 +1,13 @@
+package com.br.recomendation.recomendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecomendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecomendationApplication.class, args);
+	}
+
+}
