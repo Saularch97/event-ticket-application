ce8347ab4a6bfa4048ff7d3bfb9ed8d398f6c907
Saularch97 <duart.herrera@gmail.com>
2025-05-27 16:51:48 -0300

    saving in mongo db functional

--- recomendation/src/main/java/com/br/recomendation/recomendation/consumer/EventRequestConsumer.java
+++ recomendation/src/main/java/com/br/recomendation/recomendation/consumer/EventRequestConsumer.java
@@ -3,8 +3,12 @@ package com.br.recomendation.recomendation.consumer;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import com.br.recomendation.recomendation.services.RecomendationService;
+
 @Component
 public class EventRequestConsumer {
+
+    private final RecomendationService service;
 
     @RabbitListener(queues = { "event-request-queue" })
     public void receiveMessage(EventRequestDto message) {
@@ -12,8 +16,15 @@ public class EventRequestConsumer {
             // EventRequestDto dto = objectMapper.readValue(message, EventRequestDto.class);
             System.out.println(">>> Mensagem recebida na event-request-queue:");
             System.out.println(message + "\n");
+            
+            service.saveEventData(message);
+
         } catch (Exception e) {
             System.err.println("Erro ao desserializar a mensagem: " + e.getMessage());
         }
+    }
+
+    public EventRequestConsumer(RecomendationService service) {
+        this.service = service;
     }
 }
