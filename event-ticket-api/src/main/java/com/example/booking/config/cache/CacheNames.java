eeea2b3f66926c1cf908cce159c8c0c80d66ac60
Saularch97 <duart.herrera@gmail.com>
2025-07-08 20:00:34 -0300

    refactor in class CacheNames

--- event-ticket-api/src/main/java/com/example/booking/config/cache/CacheNames.java
+++ event-ticket-api/src/main/java/com/example/booking/config/cache/CacheNames.java
@@ -1,6 +1,15 @@
 package com.example.booking.config.cache;
 
-public class CacheNames {
+public final class CacheNames {
+    /**
+     * Utility class that holds cache name constants used across the application.
+     *
+     * This class is preferred over an enum because cache names must be string literals
+     * in annotations like @Cacheable, and enum methods (like .value()) are not allowed there.
+     *
+     * This class cannot be instantiated or extended.
+     */
+    private CacheNames(){}
     public static final String REMAINING_TICKETS = "AVAILABLE_TICKETS_CACHE";
     public static final String ORDERS = "ORDERS_CACHE";
     public static final String TOP_EVENTS = "TOP_EVENTS_CACHE";
